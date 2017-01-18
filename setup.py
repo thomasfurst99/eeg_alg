722551b12d9dc0592031371a4acf6f7028c140f0
thomasfurst99 <tomasfurst99@gmail.com>
2017-01-18 21:20:43 +0100

    Adding .gitignore

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup
 setup(name="eeg_alg",
       version="0.1",
       description="Algorithms used for processing and analyzing EEG signals.",
-      url=)
+      url="https://github.com/thomasfurst99/eeg_alg")
